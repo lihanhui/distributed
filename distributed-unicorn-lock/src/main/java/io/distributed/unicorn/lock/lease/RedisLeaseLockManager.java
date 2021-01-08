0cd7f0f405146a886d3cda190ac70a30ff4a8eca
wxtsuper <wxtsuper@163.com>
2021-01-08 11:37:43 +0800

    constructor

--- distributed-unicorn-lock/src/main/java/io/distributed/unicorn/lock/lease/RedisLeaseLockManager.java
+++ distributed-unicorn-lock/src/main/java/io/distributed/unicorn/lock/lease/RedisLeaseLockManager.java
@@ -1,6 +1,8 @@
 package io.distributed.unicorn.lock.lease;
 
+import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 
 import io.doraemon.distributed.lock.AbstractLeaseLockManager;
 import io.doraemon.distributed.lock.LeaseLock;
@@ -9,6 +11,13 @@ public class RedisLeaseLockManager extends AbstractLeaseLockManager{
 	private RedissonClient redissonClient;
 	public RedisLeaseLockManager(RedissonClient redissonClient){
 		this.redissonClient = redissonClient;
+	}
+	public RedisLeaseLockManager(String... clusterNodes){//"redis://127.0.0.1:7000", "redis://127.0.0.1:7001"
+		Config config = new Config();
+		config.useClusterServers()
+		    .setScanInterval(2000) // 集群状态扫描间隔时间，单位是毫秒
+		    .addNodeAddress(clusterNodes);//可以用"rediss://"来启用SSL连接
+		this.redissonClient = Redisson.create(config);
 	}
 	public LeaseLock getLeaseLock(String name) {
 		return new RedisLeaseLock(redissonClient.getLock(name));
